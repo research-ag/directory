97a144d5c35b29edc8bda116eb5963c337cc8125
Denys Kushnarov <154005444+reginleif888@users.noreply.github.com>
2024-03-01 08:39:25 +0100

    Display ledger principal, freezing period

--- frontend/components/root/root.tsx
+++ frontend/components/root/root.tsx
@@ -1,18 +1,39 @@
-import { useState } from "react";
-import { Box, Tabs, TabList, Tab, Typography } from "@mui/joy";
+import { useMemo, useState } from "react";
+import { format, differenceInHours, formatDuration } from "date-fns";
+import { Box, Tabs, TabList, Tab } from "@mui/joy";
 
 import Tokens from "@fe/components/tokens";
 import Owners from "@fe/components/owners";
 import ConnectButton from "@fe/components/connect-button";
 import ThemeButton from "@fe/components/theme-button";
 import { useIdentity } from "@fe/integration/identity";
+import { useFreezingPeriod } from "@fe/integration";
+
+import InfoItem from "./info-item";
 
 const Root = () => {
   const [tabValue, setTabValue] = useState(0);
 
   const { identity } = useIdentity();
 
-  const principal = identity.getPrincipal().toText();
+  const userPrincipal = identity.getPrincipal().toText();
+
+  const ledgerPrincipal = "rqx66-eyaaa-aaaap-aaona-cai";
+
+  const { data: freezingPeriod } = useFreezingPeriod();
+
+  const freezingPeriodString = useMemo(() => {
+    if (!freezingPeriod) {
+      return "N/A";
+    }
+
+    const hours = differenceInHours(
+      new Date(Date.now() + Number(freezingPeriod) / 1_000_000),
+      new Date()
+    );
+
+    return formatDuration({ hours });
+  }, [freezingPeriod]);
 
   return (
     <Box
@@ -28,8 +49,24 @@ const Root = () => {
         value={tabValue}
         onChange={(_, value) => setTabValue(value as number)}
       >
-        <Box sx={{ textAlign: "right", marginBottom: 1 }}>
-          <Typography level="body-xs">Your principal: {principal}</Typography>
+        <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-end",
+              gap: 0.5,
+              marginBottom: 1,
+            }}
+          >
+            <InfoItem label="Your principal" content={userPrincipal} withCopy />
+            <InfoItem
+              label="Ledger's principal"
+              content={ledgerPrincipal}
+              withCopy
+            />
+            <InfoItem label="Freezing period" content={freezingPeriodString} />
+          </Box>
         </Box>
         <Box
           sx={{
