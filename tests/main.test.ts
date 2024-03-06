8174295df9ae8bfc1e9a5a30f76a099894324a67
Denys <154005444+reginleif888@users.noreply.github.com>
2024-03-06 11:10:59 +0100

    Implement CI (#7)
    * Check if backend builds
    * Check did files
    * Run integration test
    * Check if frontend builds

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -81,7 +81,7 @@ describe("Directory", () => {
     actor.setIdentity(userIdentity2);
     const freezingPeriod = await actor.freezingPeriod();
     expect(!Number.isNaN(Number(freezingPeriod))).toEqual(true);
-    expect(Number(freezingPeriod) === 86_400_000_000_000).toEqual(true);
+    expect(Number(freezingPeriod) === 365 * 86_400_000_000_000).toEqual(true);
   });
 
   test("should allow owners to add new owner", async () => {
